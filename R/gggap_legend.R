8a701f661e9cbfa9d05d989b0d609e27a886cc1e
Carlos Morales <carlosmorales777@gmail.com>
2020-11-14 20:59:12 +0000

    Apply the tidyverse style guide to the aux function

--- R/gggap_legend.R
+++ R/gggap_legend.R
@@ -1,5 +1,5 @@
-#' @title Add Legend to gg.gap()
-#' @description Add legend to gg.gap().
+#' @title Add Legend to gggap()
+#' @description Adds a legend to a gggap().
 #' @param plot A 'ggplot2' plot.
 #' @param margin Margins around the text.
 #'
@@ -13,14 +13,14 @@
 #'     geom_bar() +
 #'     ggtitle("Number of Cars by Gear") +
 #'     xlab("Gears")
-#' gg.gap(plot = bp,
+#' gggap(plot = bp,
 #'        ylim = c(0,16),
 #'        segments = c(6,8))
-#' add.legend(plot = bp,
+#' gggap_legend(plot = bp,
 #'            margin = c(top=1,right=1,bottom=1,left=460))
-add.legend <- function(plot,margin=c(top=200,right=200,bottom=200,left=200)){
-    legend <- cowplot::get_legend(plot+theme(legend.box.margin =
-                                     margin(margin)))
-    grid::grid.draw(x = legend,recording=TRUE)
+gggap_legend <- function(plot,
+margin = c(top = 200, right = 200, bottom = 200, left = 200)) {
+  legend <- cowplot::get_legend(plot +
+  theme(legend.box.margin = margin(margin)))
+  grid::grid.draw(x = legend, recording = TRUE)
 }
-
